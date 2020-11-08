39df72fd3867d6cbfd2ba9ce2f740a1f5236d132
kendraleighk <71277388+kendraleighk@users.noreply.github.com>
2020-11-08 16:27:00 -0500

    Added dummy Forecast

--- src/app.js
+++ src/app.js
@@ -41,7 +41,7 @@ function displayTemperature(response) {
 
 }
 
-function search(city) {
+function search(response) {
     let apiKey = "196f4ddac8abc59085a4e103c51df6af";
     let city = "New York";
     let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
